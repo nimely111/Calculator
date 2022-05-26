a477b1af59e6f93834f9da4c83ad552c2c041b98
nimely111 <samuelehnimely@gmail.com>
2022-05-26 16:24:12 +0000

    added all class of the same name  to all operation btns

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const clear = document.getElementById('clear');
 const decimal = document.getElementById('decimal');
 //Selecting the buttons with all the 10 digits (0-9)
 const btnNumbers = document.getElementsByClassName('btn-number');
-
+//Selecting the four operators (add/subtract/divide/multiply) and the equals operators
