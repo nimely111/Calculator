492c56ccfaed1c8707527fdb83d8015f85feb048
nimely111 <samuelehnimely@gmail.com>
2022-05-26 16:01:45 +0000

    remove ths s  from all btn-number and select them in the javascript

--- script.js
+++ script.js
@@ -3,5 +3,6 @@
 const displayEleValue = document.getElementById('screen');
 const clear = document.getElementById('clear');
 const decimal = document.getElementById('decimal');
-//Selecting the buttons with all the 10 digits
+//Selecting the buttons with all the 10 digits (0-9)
+const btnNumbers = document.getElementsByClassName('btn-number');
 
