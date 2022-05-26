4f047811a7289c7d861a429f3b7602bce6478e7b
nimely111 <samuelehnimely@gmail.com>
2022-05-26 16:56:02 +0000

    select all the buttons that contains the operations

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const clear = document.getElementById('clear');
 const decimal = document.getElementById('decimal');
 //Selecting the buttons with all the 10 digits (0-9)
 const btnNumbers = document.getElementsByClassName('btn-number');
+//Selecting the four operators (add/subtract/divide/multiply) and the equals operators
+const btnOperators = document.getElementsByClassName('btn-operator');
